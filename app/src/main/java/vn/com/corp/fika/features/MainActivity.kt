84d0c320166a2a0c9698fe7fb158f3dcde5a5248
Long Nguyen <long.nguyen@mingle.com>
2020-08-02 12:00:18 +0700

    [f] Refactor code

--- app/src/main/java/vn/com/corp/fika/features/MainActivity.kt
+++ app/src/main/java/vn/com/corp/fika/features/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
@@ -17,6 +18,7 @@ import vn.com.corp.fika.R
 import vn.com.corp.fika.adapter.GridRecyclerAdapter
 import vn.com.corp.fika.databinding.ActivityMainBinding
 import vn.com.corp.fika.util.AdapterImageSlider
+import vn.com.corp.fika.util.ProgressDialog
 import vn.com.corp.fika.util.SpaceItemDecoration
 import vn.com.corp.fika.util.Utils
 import vn.com.corp.fika.viewmodel.MainViewModel
@@ -26,8 +28,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mViewBinding: ActivityMainBinding
     private val viewModel by viewModels<MainViewModel>()
-    private val imageAdapter: AdapterImageSlider by lazy { AdapterImageSlider() }
-    private val adapter by lazy { GridRecyclerAdapter() }
+    private val imageAdapter by lazy { AdapterImageSlider() }
+    private val momentAdapter by lazy { GridRecyclerAdapter() }
+    private val loading by lazy { ProgressDialog.progressDialog(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         viewModel.getUserProfile()
+        loading.show()
         mViewBinding.recyclerView.layoutManager = GridLayoutManager(this, 2).also {
             it.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
                 override fun getSpanSize(position: Int): Int {
@@ -52,7 +56,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         mViewBinding.recyclerView.addItemDecoration(SpaceItemDecoration(5));
-        mViewBinding.recyclerView.adapter = adapter
+        mViewBinding.recyclerView.adapter = momentAdapter
         mViewBinding.imgHeart.setOnClickListener {
             (it as ImageView).setImageResource(R.drawable.ic_filled_favorite_24)
             val scaleDown: ValueAnimator? = ObjectAnimator.ofPropertyValuesHolder(
@@ -84,8 +88,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpViewModels() {
         viewModel.userProfileLiveData.observe(this, Observer {
+            loading.hide()
             mViewBinding.data = it
             mViewBinding.executePendingBindings()
+            mViewBinding.mainContainer.isVisible = true
             it.profile?.images?.let { imageList ->
                 imageAdapter.setItems(imageList.filterNotNull())
                 mViewBinding.pager.adapter = imageAdapter
@@ -97,8 +103,10 @@ class MainActivity : AppCompatActivity() {
                 val old = Utils.getYearOld(dobAsLong)
                 mViewBinding.tvDob.text = String.format(getString(R.string.dob_holder), dob, old)
             }
-            //TODO: moment is a list so make a recyclerView hold list of moment, then item is a Grid recyclerView
-            adapter.setData(it.profile?.moment?.get(0))
+            /**
+             * TODO: moment is a list so make a bigger recyclerView hold list of moment, then item is a small recyclerView in there
+             */
+            momentAdapter.setData(it.profile?.moment?.get(0))
         })
     }
 
@@ -107,8 +115,8 @@ class MainActivity : AppCompatActivity() {
         val customDialog = AlertDialog.Builder(this)
             .setView(dialogView)
             .show()
-        val btDismiss = dialogView.findViewById<Button>(R.id.btDismissCustomDialog)
-        btDismiss.setOnClickListener {
+        val btnDismiss = dialogView.findViewById<Button>(R.id.btDismissCustomDialog)
+        btnDismiss.setOnClickListener {
             customDialog.dismiss()
         }
     }
